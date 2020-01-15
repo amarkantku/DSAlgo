f3884c75bcceab001a71d2f3ed1f2a9fc8bfd6bf
Amarkant Kumar <amar.du2013@gmail.com>
2020-01-15 23:17:19 +0530

    Singly Link List Added

---
+++ SinglyLinkedList/Node.js
@@ -0,0 +1,6 @@
+export default class Node {
+    constructor(params) {
+        this.element = params;
+        this.next = null;
+    }
+};
---
+++ SinglyLinkedList/SinglyLinkedList.js
@@ -0,0 +1,38 @@
+import Node from './Node'
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    add(element){
+        const node = new Node(element);
+        if (!this.head) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            while(current.next !== null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
+
+    print() {
+        let current = this.head;
+        let string = '';
+        while(current) {
+            string += current.element + ' ';
+            current = current.next;
+        }
+        console.log(string);
+    }
+}
+
+const SLinkedList = new SinglyLinkedList();
+SLinkedList.add(1);
+SLinkedList.add(2);
+SLinkedList.add(3);
+SLinkedList.add(4);
+SLinkedList.print();
